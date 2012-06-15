5bdba186d62b341197a5771641d2bdf7040a8f19
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2012-06-15 22:31:37 +0200

    Use ActiveModel Badge, not Ambry's.

--- lib/merit/models/active_record/badge.rb
+++ lib/merit/models/active_record/badge.rb
@@ -1,27 +1,4 @@
-require 'ambry'
-require 'ambry/active_model'
-
-class Badge
-  extend Ambry::Model
-  extend Ambry::ActiveModel
-
-  field :id, :name, :level, :image, :description
-
-  validates_presence_of :id, :name
-  validates_uniqueness_of :id
-
-  filters do
-    def find_by_id(ids)
-      ids = ids.kind_of?(Array) ? ids : [ids]
-      find{|b| ids.include? b.id }
-    end
-    def by_name(name)
-      find{|b| b.name == name.to_s }
-    end
-    def by_level(level)
-      find{|b| b.level.to_s == level.to_s }
-    end
-  end
+class Badge < ActiveRecord::Base
 
   # Grant badge to sash
   def grant_to(object_or_sash)
@@ -42,4 +19,5 @@ class Badge
       return false
     end
   end
+
 end
